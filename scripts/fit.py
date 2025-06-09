a9906c06821187e9a5e97fd663c55971d2fa618d
kruglikovAlex <kruglikovalex280577@gmail.com>
2025-06-09 21:21:37 +0000

    add new branch with new yaml files

---
+++ scripts/fit.py
@@ -0,0 +1,57 @@
+# scripts/fit.py
+
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+#from category_encoders import CatBoostEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+#from catboost import CatBoostClassifier
+import yaml
+import os
+import joblib
+
+# обучение модели
+def fit_model():
+    # Прочитаем файл с гиперпараметрами params.yaml
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd) 
+
+    # загружаем результат предыдущего шага: inital_data.csv
+    data = pd.read_csv('data/initial_data.csv')
+
+    # обучение модели
+    cat_features = data.select_dtypes(include='object')
+    potential_binary_features = cat_features.nunique() == 2
+
+    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
+    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
+    num_features = data.select_dtypes(['float'])
+
+    preprocessor = ColumnTransformer(
+        [
+            ('binary', OneHotEncoder(drop=params['one_hot_drop']), binary_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(drop=params['one_hot_drop']), other_cat_features.columns.tolist()),
+            ('num', StandardScaler(), num_features.columns.tolist())
+        ],
+        remainder='drop',
+        verbose_feature_names_out=False
+    )
+
+    model = LogisticRegression(C=params['C'] , penalty=params['penalty'])
+
+    pipeline = Pipeline(
+        [
+            ('preprocessor', preprocessor),
+            ('model', model)
+        ]
+    )
+    pipeline.fit(data, data[params['target_col']]) 
+
+    # сохранение результата шага
+    os.makedirs('models', exist_ok=True) # создание директории, если её ещё нет
+    with open('models/fitted_model.pkl', 'wb') as fd:
+        joblib.dump(pipeline, fd) 
+
+if __name__ == '__main__':
+	fit_model()
